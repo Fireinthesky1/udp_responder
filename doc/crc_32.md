fad8f2d6f7c01578d68d4c52994e53b946a24af2
James Hicks <james.hicks01234@gmail.com>
2026-04-29 22:00:45 -0700

    Implemented control byte computation, slight addition to crc doc

--- doc/crc_32.md
+++ doc/crc_32.md
@@ -7,8 +7,12 @@ The `crc_32` module computes the CRC value by performing the following steps:
 2. Perform modulo-2 binary division.
 3. Store the remainder of the modulo-2 binary division as the CRC.
 
+
 ## Implementation
-TODO
+The algorithm used is the algorithm described by Ross N. Williams in
+"A painless guide to CRC error detection algorithms". The CRC register flows
+from right to left.
+
 
 ## Timing
 TODO: Latency
--- src/crc_32.sv
+++ src/crc_32.sv
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  <32 bit CRC with parameterized polynomial and seed.>
+//  <32 bit CRC with parameterized polynomial.>
 //  Copyright (C) 2026 James Hicks
 //
 //  This program is free software: you can redistribute it and/or modify
@@ -18,7 +18,7 @@
 // DETAILS
 //
 // Responsibilities of this component are the following
-//   -- Take in 8-bit data and output 32-bit CRC.
+//   -- Take in 8-bit data and output 32-bit CRC 1 clock cycle later.
 ////////////////////////////////////////////////////////////////////////////////
 
 module crc_32
@@ -38,4 +38,34 @@ module crc_32
   output logic [31:0] crc_out
   );
 
+
+  // Return the control byte given an input byte
+  function logic[7:0] compute_control_byte (input logic[7:0] top_byte);
+    logic [7:0] control_byte;
+
+    control_byte[7] = input[7];
+    control_byte[6] = control_byte[7] ^ input[6];
+    control_byte[5] = control_byte[6] ^ input[5];
+    control_byte[4] = control_byte[5] ^ input[4];
+    control_byte[3] = control_byte[4] ^ input[3];
+    control_byte[2] = control_byte[3] ^ input[2];
+    control_byte[1] = control_byte[2] ^ input[1];
+    control_byte[0] = control_byte[1] ^ input[0];
+
+    return control_byte;
+  endfunction // compute_control_byte
+
+  // Returns a logic array created by summing the POLYNOMIAL at different
+  // offsets dictated by the input control byte.
+  function logic [31:0] compute_summed_poly (input logic[7:0] control_byte);
+    logic [31:0] summed_poly;
+    // TODO: Implement
+  endfunction // compute_summed_poly
+
+
+  logic [32:0] crc_reg;
+
+  always_ff @(posedge clk) begin
+    // TODO
+  end
 endmodule // crc_32
